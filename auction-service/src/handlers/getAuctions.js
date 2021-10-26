2f562f107e466d12accf53f5a1a62f60bb1c1502
Franck Kabasele <kabasele467@gmail.com>
2021-10-26 12:52:09 -0400

    add functions to retrieve all auctions

--- auction-service/src/handlers/getAuctions.js
+++ auction-service/src/handlers/getAuctions.js
@@ -4,36 +4,25 @@ import createError from 'http-errors';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-export async function getAuctionById(id) {
-  let auction;
+async function getAuctions(id) {
+  let auctions;
 
   try {
-    const result = await dynamodb.get({
+    const result = await dynamodb.scan({
       TableName: process.env.AUCTIONS_TABLE_NAME,
       Key: { id },
     }).promise();
 
-    auction = result.Item;
+    auctions = result.Items;
   } catch (error) {
     console.error(error);
     throw new createError.InternalServerError(error);
   }
 
-  if (!auction) {
-    throw new createError.NotFound(`Auction with ID "${id}" not found!`);
-  }
-
-  return auction;
-}
-
-async function getAuction(event, context) {
-  const { id } = event.pathParameters;
-  const auction = await getAuctionById(id);
-
   return {
     statusCode: 200,
-    body: JSON.stringify(auction),
+    body: JSON.stringify(auctions),
   };
 }
 
-export const handler = getAuction;
+export const handler = commonMiddleware(getAuctions);
