d811702fbcd8e415dac8d89446e87aeb200ce84a
Franck Kabasele <kabasele467@gmail.com>
2021-10-26 17:20:30 -0400

    add schema validation

--- auction-service/src/handlers/placeBid.js
+++ auction-service/src/handlers/placeBid.js
@@ -1,45 +1,70 @@
 import AWS from 'aws-sdk';
-import commonMiddleware from '../lib/commonMiddleware';
 import createError from 'http-errors';
+import validator from '@middy/validator';
 import { getAuctionById } from './getAuction';
+import commonMiddleware from '../lib/commonMiddleware';
+import placeBidSchema from '../lib/schemas/placeBidSchema';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function placeBid(event, context) {
- const { id } = event.pathParameters;
- const { amount } = event.body;
- 
- const auction = await getAuctionById(id); 
+  const { id } = event.pathParameters;
+  const { amount } = event.body;
+  const { email } = event.requestContext.authorizer;
 
- if(amount <= auction.highestBid.amount) {
-     throw new createError.Forbidden(`Your bid must be higher than ${auction.highestBid.amount}!`);
- }
+  const auction = await getAuctionById(id);
 
- const params = {
-     TableName: process.env.AUCTIONS_TABLE_NAME,
-     Key: { id },
-     UpdateExpression: 'set highestBid.amount = :amount',
-     ExpressionAttributeValues: {
-         ':amount': amount,
-     },
-     ReturnValue: 'ALL_NEW',
- };
+  // Bid identity validation
+  if (email === auction.seller) {
+    throw new createError.Forbidden(`You cannot bid on your own auctions!`);
+  }
 
- let updatedAuction;
+  // Avoid double bidding
+  if (email === auction.highestBid.bidder) {
+    throw new createError.Forbidden(`You are already the highest bidder`);
+  }
 
- try {
+  // Auction status validation
+  if (auction.status !== 'OPEN') {
+    throw new createError.Forbidden(`You cannot bid on closed auctions!`);
+  }
+
+  // Bid amount validation
+  if (amount <= auction.highestBid.amount) {
+    throw new createError.Forbidden(`Your bid must be higher than ${auction.highestBid.amount}!`);
+  }
+
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    Key: { id },
+    UpdateExpression: 'set highestBid.amount = :amount, highestBid.bidder = :bidder',
+    ExpressionAttributeValues: {
+      ':amount': amount,
+      ':bidder': email,
+    },
+    ReturnValues: 'ALL_NEW',
+  };
+
+  let updatedAuction;
+
+  try {
     const result = await dynamodb.update(params).promise();
     updatedAuction = result.Attributes;
- } catch (error){
-     console.log(error);
-     throw new createError.InternalServerError(error);
- }
- 
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
 
-return {
+  return {
     statusCode: 200,
     body: JSON.stringify(updatedAuction),
-};
+  };
 }
 
-export const handler = commonMiddleware(placeBid);
+export const handler = commonMiddleware(placeBid)
+  .use(validator({ 
+      inputSchema: placeBidSchema,     
+      ajvOptions: {
+            useDefaults: true,
+            strict: false,
+  }}));
