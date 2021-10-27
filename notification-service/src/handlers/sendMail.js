c5115d07536c629f7fe12833dcff3bfef14c6bee
Franck Kabasele <kabasele467@gmail.com>
2021-10-27 15:27:20 -0400

    update notification service

--- notification-service/src/handlers/sendMail.js
+++ notification-service/src/handlers/sendMail.js
@@ -3,19 +3,25 @@ import AWS from 'aws-sdk';
 const ses = new AWS.SES({ region: 'us-east-1' });
 
 async function sendMail(event, context) {
+  const record = event.Records[0];
+  console.log('record processing', record);
+
+  const email = JSON.parse(record.body);
+  const {subject, body, recipient } = email;
+
   const params = {
     Source: 'kabasele467@gmail.com',
     Destination: {
-      ToAddresses: ['kabasele467@gmail.com'],
+      ToAddresses: [recipient],
     },
     Message: {
       Body: {
         Text: {
-          Data: 'Hello from Congo!',
+          Data: body,
         },
       },
       Subject: {
-        Data: 'Test Mail',
+        Data: subject,
       },
     },
   };
